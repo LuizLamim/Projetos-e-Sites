bf562b2754610a1763288f21e78b7010c4d0e353
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-07 20:16:19 -0300

    Bot Events

--- botdiscord.py
+++ botdiscord.py
@@ -4,3 +4,9 @@ from discord.ext import commands
 # Configuração de permissões (Intents)
 intents = discord.Intents.default()
 intents.message_content = True  # Necessário para ler o conteúdo das mensagens
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot logado como {bot.user.name}')
