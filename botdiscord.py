0adf0a6fedf2589dc0b747c045565d3266016b34
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-07 20:16:00 -0300

    CFG

--- botdiscord.py
+++ botdiscord.py
@@ -1,2 +1,6 @@
 import discord
 from discord.ext import commands
+
+# Configuração de permissões (Intents)
+intents = discord.Intents.default()
+intents.message_content = True  # Necessário para ler o conteúdo das mensagens
