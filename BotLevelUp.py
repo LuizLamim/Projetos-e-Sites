d1ed4a5bf54989bb2bc9c3934d477ef5e54e03b5
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-11 19:39:43 -0300

    Básico

--- BotLevelUp.py
+++ BotLevelUp.py
@@ -1,3 +1,9 @@
 import json
 from steampy.client import SteamClient
 from steampy.utils import GameOptions
+
+class SteamLevelUpBot:
+    def __init__(self, api_key, username, password, steam_guard_file):
+        self.api_key = api_key
+        self.username = username
+        self.password = password
