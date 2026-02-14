d1805e950001e3c5e1dcaa73af98fb495ce71f37
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-13 21:56:26 -0300

    Partículas

--- particulas.py
+++ particulas.py
@@ -1,3 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+
+# Configurações iniciais
+N_PARTICULAS = 25
+TAMANHO_TANQUE = 10
+RAIO_PARTICULA = 0.3
+VELOCIDADE_INICIAL = 0.15
