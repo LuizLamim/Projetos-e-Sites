b7245ef996dd66d81672271857aa1621eb3d6210
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-16 20:11:56 -0300

    Gerenciar vanco

--- bancodedadossqlite.py
+++ bancodedadossqlite.py
@@ -1,1 +1,9 @@
 import sqlite3
+
+def gerenciar_banco():
+    # 1. Cria a conexão com o banco de dados
+    # Se o arquivo 'meu_banco.db' não existir, ele será criado automaticamente.
+    conexao = sqlite3.connect('meu_banco.db')
+    
+    # 2. Cria o cursor (o objeto que executa os comandos SQL)
+    cursor = conexao.cursor()
