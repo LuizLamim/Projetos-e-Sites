37d42bc40ce3dd3cdfe54bbff368abe3cc578942
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-15 21:42:32 -0300

    3

--- lnx.py
+++ lnx.py
@@ -6,3 +6,7 @@ import matplotlib.pyplot as plt
 # Começamos de 0.1 para evitar erros matemáticos e mostrar a curva descendo.
 x = np.linspace(0.1, 10, 400)
 y = np.log(x)
+
+# 2. Criar a figura e o gráfico
+plt.figure(figsize=(8, 5))
+plt.plot(x, y, label=r'$f(x) = \ln(x)$', color='blue', linewidth=2)
