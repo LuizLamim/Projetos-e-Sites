c14c947555b7c70f85c3e026182ded992419da45
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-17 20:37:35 -0300

    Erros

--- coeficienteangular.py
+++ coeficienteangular.py
@@ -1,3 +1,15 @@
 def calcular_coeficiente_angular():
     print("--- Calculadora de Coeficiente Angular ---")
     print("Insira as coordenadas do Ponto 1 (x1, y1):")
+
+    try:
+        x1 = float(input("x1: "))
+        y1 = float(input("y1: "))
+        
+        print("\nInsira as coordenadas do Ponto 2 (x2, y2):")
+        x2 = float(input("x2: "))
+        y2 = float(input("y2: "))
+        
+        # Cálculo das variações (Deltas)
+        delta_y = y2 - y1
+        delta_x = x2 - x1
