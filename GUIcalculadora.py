11e1d0481646a4881a954c646e628459442b7446
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-18 21:08:44 -0300

    Class

--- GUIcalculadora.py
+++ GUIcalculadora.py
@@ -1,2 +1,20 @@
 import tkinter as tk
 from tkinter import messagebox
+
+class Calculadora:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Calculadora Python")
+        self.root.geometry("300x400")
+        self.root.resizable(False, False)
+
+        # Variável para armazenar o texto do visor
+        self.expressao = ""
+        self.texto_entrada = tk.StringVar()
+
+        # Configuração do Visor
+        entrada = tk.Entry(self.root, textvariable=self.texto_entrada, font=('Arial', 24), bd=10, insertwidth=2, width=14, borderwidth=4, justify="right")
+        entrada.grid(row=0, column=0, columnspan=4)
+
+        # Definição dos Botões
+        # Layout: (Texto do botão, Linha, Coluna)
