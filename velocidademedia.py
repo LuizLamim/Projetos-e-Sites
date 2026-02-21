8326a69b511601f5eab747a9466eb49dc8fbd895
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-21 19:07:23 -0300

    Rececendo os dados

--- velocidademedia.py
+++ velocidademedia.py
@@ -1,2 +1,7 @@
 def calcular_velocidade_media():
     print("--- Calculadora de Velocidade Média ---")
+
+    try:
+        # Recebendo os dados do usuário
+        distancia = float(input("Digite a distância percorrida (em km): "))
+        tempo = float(input("Digite o tempo gasto (em horas): "))
