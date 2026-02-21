4c685868ecd61b1b678df1dba511c2e1bde2f797
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-20 22:26:53 -0300

    Verificar

--- reior.py
+++ reior.py
@@ -2,3 +2,12 @@ import math
 
 def calcular_perimetro():
     print("--- Calculadora de Perímetro de Circunferência ---")
+
+    try:
+        # 1. Recebe o valor do raio digitado pelo usuário
+        raio_str = input("Digite o valor do raio: ")
+        raio = float(raio_str) # Converte o texto para número decimal
+        
+        if raio < 0:
+            print("O raio não pode ser um número negativo.")
+            return
