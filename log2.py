71dce8805d25731f28891685f938d85fdabb5c6d
LuizLamim <luiz.britohenrique@hotmail.com>
2026-03-03 21:52:04 -0300

    2

--- log2.py
+++ log2.py
@@ -4,3 +4,6 @@ import numpy as np
 # 1. Definir o intervalo de x (x deve ser maior que 0 para logaritmos)
 # Vamos de 0.1 até 10 para evitar o erro de log(0)
 x = np.linspace(0.1, 10, 400)
+
+# 2. Calcular o logaritmo na base 2
+y = np.log2(x)
