f6e5cd0a93b1d898f6ec0057895e7df8501765cc
LuizLamim <luiz.britohenrique@hotmail.com>
2026-03-03 21:51:54 -0300

    Intervalo

--- log2.py
+++ log2.py
@@ -1,2 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+# 1. Definir o intervalo de x (x deve ser maior que 0 para logaritmos)
+# Vamos de 0.1 até 10 para evitar o erro de log(0)
+x = np.linspace(0.1, 10, 400)
