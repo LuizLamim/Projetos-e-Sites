35c48d9589dd9fce8b5415fe08f244599091eceb
LuizLamim <luiz.britohenrique@hotmail.com>
2026-03-05 20:14:49 -0300

    2

--- Soma5.py
+++ Soma5.py
@@ -3,3 +3,5 @@ n2 = float(input("Digite o 2º número: "))
 n3 = float(input("Digite o 3º número: "))
 n4 = float(input("Digite o 4º número: "))
 n5 = float(input("Digite o 5º número: "))
+
+soma = n1 + n2 + n3 + n4 + n5
