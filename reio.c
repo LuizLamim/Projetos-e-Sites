31d5445857599e6802237c5a5ff90a23a3454fc6
LuizLamim <luiz.britohenrique@hotmail.com>
2026-03-11 22:32:11 -0300

    Scanf

--- reio.c
+++ reio.c
@@ -7,5 +7,8 @@ int main() {
     // Definição da constante Pi
     const double PI = 3.14159265359;
 
+    printf("Digite o valor do raio da circunferencia: ");
+    scanf("%lf", &raio);
+
     return 0;
 }
