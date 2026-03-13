b101c6f476d30eedb2fff58d3ab380566cf375a7
LuizLamim <luiz.britohenrique@hotmail.com>
2026-03-12 23:04:19 -0300

    3

--- Perceptron.py
+++ Perceptron.py
@@ -6,3 +6,13 @@ class Perceptron:
         self.n_iter = n_iterations
         self.weights = None
         self.bias = None
+
+    def activation_function(self, x):
+        # Função de Degrau: retorna 1 se x >= 0, caso contrário 0
+        return 1 if x >= 0 else 0
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        # Inicializa pesos com zero
+        self.weights = np.zeros(n_features)
+        self.bias = 0
