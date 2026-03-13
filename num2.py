b3515752d9de911c51ffcc1bf9aab0b08f40c39f
LuizLamim <luiz.britohenrique@hotmail.com>
2026-03-13 20:15:53 -0300

    4

--- num2.py
+++ num2.py
@@ -2,3 +2,5 @@ num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
 resultado = num1 * num2
+
+print(f"O resultado de {num1} x {num2} é: {resultado}")
