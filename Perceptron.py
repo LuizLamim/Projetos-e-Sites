3e4adc2a41223a498b93e323d8b93f282adb4fd4
LuizLamim <luiz.britohenrique@hotmail.com>
2026-03-12 23:04:01 -0300

    2

--- Perceptron.py
+++ Perceptron.py
@@ -1,1 +1,8 @@
 import numpy as np
+
+class Perceptron:
+    def __init__(self, learning_rate=0.1, n_iterations=100):
+        self.lr = learning_rate
+        self.n_iter = n_iterations
+        self.weights = None
+        self.bias = None
