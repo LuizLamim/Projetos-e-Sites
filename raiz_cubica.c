802316003c43e3a34b3f5934efb5524cd155997f
LuizLamim <luiz.britohenrique@hotmail.com>
2026-03-17 22:27:49 -0300

    4

--- raiz_cubica.c
+++ raiz_cubica.c
@@ -3,6 +3,15 @@
 
 int main() {
     double numero, resultado;
+
+    printf("--- Calculadora de Raiz Cúbica ---\n");
+    printf("Digite um número: ");
+    
+    // Lendo a entrada do usuário
+    if (scanf("%lf", &numero) != 1) {
+        printf("Erro: Por favor, digite um número válido.\n");
+        return 1;
+    }
     
     return 0;
 
