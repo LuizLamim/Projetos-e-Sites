7dc4b3ba123e2a31d54a8f5c9431687528151d9f
LuizLamim <luiz.britohenrique@hotmail.com>
2026-04-14 23:40:56 -0300

    1

---
+++ vetorressoma.py
@@ -0,0 +1,8 @@
+def somar_vetores(v1, v2):
+    # Verifica se os vetores têm o mesmo tamanho para que a soma seja possível
+    if len(v1) != len(v2):
+        raise ValueError("Os vetores precisam ter o mesmo número de elementos.")
+    
+    # Soma os elementos de mesma posição (índice)
+    vetor_soma = [v1[i] + v2[i] for i in range(len(v1))]
+    return vetor_soma
