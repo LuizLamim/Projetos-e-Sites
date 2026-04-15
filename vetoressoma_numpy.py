9628762d1ca3e5b1fe865f29ab6a996e03d04948
LuizLamim <luiz.britohenrique@hotmail.com>
2026-04-15 00:06:31 -0300

    5

--- vetoressoma_numpy.py
+++ vetoressoma_numpy.py
@@ -6,3 +6,5 @@ vetor_b = np.array([2, 3, 4])
 
 # O NumPy permite usar o sinal de "+" diretamente para somar os elementos
 resultado = vetor_a + vetor_b
+
+print(f"Resultado da soma (NumPy): {resultado}") # Saída: [ 3  8 13]
