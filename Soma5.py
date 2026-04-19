2a6a3fd4ae7bac34d2fa5de416fd3ab8743eeabc
LuizLamim <112779563+LuizLamim@users.noreply.github.com>
2026-04-19 20:47:55 -0300

    Atualizar o Soma5.py

--- Soma5.py
+++ Soma5.py
@@ -3,8 +3,9 @@ n2 = float(input("Digite o 2º número: "))
 n3 = float(input("Digite o 3º número: "))
 n4 = float(input("Digite o 4º número: "))
 n5 = float(input("Digite o 5º número: "))
+n6 = float(input("Digite o 6º número: "))
 
-soma = n1 + n2 + n3 + n4 + n5
+soma = n1 + n2 + n3 + n4 + n5 
 
 print(f"A soma dos 5 números é: {soma}")
 
