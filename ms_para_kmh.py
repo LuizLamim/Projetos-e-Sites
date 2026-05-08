fe3eeef9d460ed971199023a9814df637ace1230
LuizLamim <luiz.britohenrique@hotmail.com>
2026-05-07 23:11:38 -0300

    Interface simples para o usuário

--- ms_para_kmh.py
+++ ms_para_kmh.py
@@ -2,3 +2,6 @@ def converter_ms_para_kmh(velocidade_ms):
     """Converte velocidade de m/s para km/h."""
     velocidade_kmh = velocidade_ms * 3.6
     return velocidade_kmh
+
+# Interface simples para o usuário
+print("--- Conversor de Velocidade ---")
