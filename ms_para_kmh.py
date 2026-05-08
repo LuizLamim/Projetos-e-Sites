cd6f405e6c5e05aeefe34014629aefc1ee737e31
LuizLamim <luiz.britohenrique@hotmail.com>
2026-05-07 23:12:05 -0300

    Try

--- ms_para_kmh.py
+++ ms_para_kmh.py
@@ -5,3 +5,8 @@ def converter_ms_para_kmh(velocidade_ms):
 
 # Interface simples para o usuário
 print("--- Conversor de Velocidade ---")
+
+try:
+    valor_ms = float(input("Digite a velocidade em m/s: "))
+    
+    resultado = converter_ms_para_kmh(valor_ms)
