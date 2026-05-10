6591327da37f207224655deb0ff8288aa269c1b2
LuizLamim <luiz.britohenrique@hotmail.com>
2026-05-09 23:10:47 -0300

    Calcular os valores de y

--- plot_sen_x_plus_cos_x.py
+++ plot_sen_x_plus_cos_x.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 # 1. Definir o intervalo de x (de 0 a 4pi para vermos dois ciclos completos)
 # O linspace cria 400 pontos entre os valores para o gráfico ficar suave
 x = np.linspace(0, 4 * np.pi, 400)
+
+# 2. Calcular os valores de y
+y = np.sin(x) + np.cos(x)
