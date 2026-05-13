4fc1e3ac6447295f61ec2a9078f66956fac93d39
LuizLamim <luiz.britohenrique@hotmail.com>
2026-05-13 20:30:23 -0300

    Criar a tabela de usuários/pessoas

--- bancosimples.sql
+++ bancosimples.sql
@@ -1,3 +1,11 @@
 -- 1. Criar o banco de dados
 CREATE DATABASE SistemaBusca;
 USE SistemaBusca;
+
+-- 2. Criar a tabela de usuários/pessoas
+CREATE TABLE Pessoas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_completo VARCHAR(255) NOT NULL,
+    email VARCHAR(100),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
