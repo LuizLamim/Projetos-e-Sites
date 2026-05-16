a7fcab62ae825d6674a3110452cafcf48d492c2f
LuizLamim <luiz.britohenrique@hotmail.com>
2026-05-15 22:01:28 -0300

    start

--- assembly.asm
+++ assembly.asm
@@ -4,3 +4,11 @@ section .data
 
 section .text
     global _start              ; Define o ponto de entrada do programa
+
+_start:
+    ; 1. Imprimir a mensagem na tela (syscall: sys_write)
+    mov rax, 1                 ; Número da chamada de sistema para 'write' (1)
+    mov rdi, 1                 ; Descritor de arquivo: 1 é a saída padrão (stdout)
+    mov rsi, msg               ; Endereço de memória onde a mensagem começa
+    mov rdx, len               ; Quantidade de bytes a serem lidos (tamanho da mensagem)
+    syscall                    ; Pede ao sistema operacional para executar a ação
