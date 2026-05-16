531e2ea543a9f4d8b589b39fc5d914b42fd30fdf
LuizLamim <luiz.britohenrique@hotmail.com>
2026-05-15 22:01:11 -0300

    txt

--- assembly.asm
+++ assembly.asm
@@ -1,3 +1,6 @@
 section .data
     msg db "Olá, Mundo!", 0xA  ; A mensagem a ser impressa, seguida de uma quebra de linha (0xA)
     len equ $ - msg            ; Calcula automaticamente o tamanho da string
+
+section .text
+    global _start              ; Define o ponto de entrada do programa
