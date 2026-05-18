fcb36d064f58167fd13458a80ecb36a2dab19953
LuizLamim <luiz.britohenrique@hotmail.com>
2026-05-18 18:00:29 -0300

    Cria um modelo de dados para a requisição POST usando Pydantic

--- api_base.py
+++ api_base.py
@@ -6,3 +6,9 @@ app = FastAPI(
     title="Minha API Básica",
     description="Um modelo inicial de API em Python"
 )
+
+# 2. Cria um modelo de dados para a requisição POST usando Pydantic
+class Item(BaseModel):
+    nome: str
+    preco: float
+    em_estoque: bool = True # Valor padrão
