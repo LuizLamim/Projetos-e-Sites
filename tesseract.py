d15f9ef5479da127aa51c58a09b1cbade1c1aaaa
LuizLamim <luiz.britohenrique@hotmail.com>
2026-05-20 20:11:34 -0300

    Definir as 32 arestas conectando os vértices e Configurar a figura 3D do Matplotlib

--- tesseract.py
+++ tesseract.py
@@ -10,3 +10,16 @@ for i in range(16):
     z = -1 if (i & 4) == 0 else 1
     w = -1 if (i & 8) == 0 else 1
     vertices.append(np.array([x, y, z, w]))
+
+# 2. Definir as 32 arestas conectando os vértices
+# Uma aresta existe se os vértices diferem em exatamente 1 bit em binário
+edges = []
+for i in range(16):
+    for j in range(i + 1, 16):
+        if bin(i ^ j).count('1') == 1:
+            edges.append((i, j))
+
+# 3. Configurar a figura 3D do Matplotlib
+fig = plt.figure(figsize=(8, 8), facecolor='black')
+ax = fig.add_subplot(111, projection='3d')
+ax.set_facecolor('black')
