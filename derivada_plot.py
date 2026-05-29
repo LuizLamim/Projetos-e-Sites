b96777f47e18c276f5fb691129bdb76a084c6fbc
LuizLamim <luiz.britohenrique@hotmail.com>
2026-05-29 18:17:06 -0300

    Criar o gráfico

--- derivada_plot.py
+++ derivada_plot.py
@@ -7,3 +7,8 @@ x = np.linspace(-10, 10, 400)
 # 2. Definir as funções
 f = 2 * x**2 + 5
 g = 4 * x
+
+# 3. Criar o gráfico
+plt.figure(figsize=(8, 6))
+plt.plot(x, f, label='$f(x) = 2x^2 + 5$', color='blue', linewidth=2)
+plt.plot(x, g, label='$g(x) = 4x$', color='red', linestyle='--', linewidth=2)
