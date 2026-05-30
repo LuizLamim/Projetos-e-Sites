144dacf53a4d939845ccea5bdb4c5ca665e542f5
LuizLamim <luiz.britohenrique@hotmail.com>
2026-05-30 20:58:14 -0300

    Solicitar número

--- rais_quinta.py
+++ rais_quinta.py
@@ -1,3 +1,6 @@
 def calcular_raiz_quinta(numero):
     # Elevamos o número à potência de 0.2
     return numero ** 0.2
+
+# Solicitar número:
+num = float(input("Digite um número para calcular a raiz quinta: "))
