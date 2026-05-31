dccb46a253fffe7a15a8252d367ba604d5bf8b5d
LuizLamim <luiz.britohenrique@hotmail.com>
2026-05-31 00:48:18 -0300

    Entrada de dados do primeiro ponto

--- coeficiente_angular.c
+++ coeficiente_angular.c
@@ -6,5 +6,9 @@ int main() {
 
     printf("--- Calculadora de Coeficiente Angular ---\n\n");
 
+    // Entrada de dados do primeiro ponto
+    printf("Digite as coordenadas do ponto A (x1 y1): ");
+    scanf("%f %f", &x1, &y1);
+
     return 0;
 }
