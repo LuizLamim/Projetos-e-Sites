77ae0cc485b030823c7b4899f32d998e79ff7e76
LuizLamim <luiz.britohenrique@hotmail.com>
2026-05-31 00:49:02 -0300

    Validação

--- coeficiente_angular.c
+++ coeficiente_angular.c
@@ -14,5 +14,18 @@ int main() {
     printf("Digite as coordenadas do ponto B (x2 y2): ");
     scanf("%f %f", &x2, &y2);
 
+    // Validação para evitar divisão por zero (reta vertical)
+    if (x2 == x1) {
+        printf("\nErro: x1 nao pode ser igual a x2.\n");
+        printf("A reta e vertical e o coeficiente angular e indefinido.\n");
+    } else {
+        // Cálculo do coeficiente angular
+        m = (y2 - y1) / (x2 - x1);
+        
+        printf("\n-----------------------------------------\n");
+        printf("O coeficiente angular (m) da reta e: %.2f\n", m);
+        printf("-----------------------------------------\n");
+    }
+
     return 0;
 }
