f2e94e872b753f49eac1619e03d5cc907873739e
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-02 13:39:16 -0300

    Entrada do usuário

--- esfera_area.py
+++ esfera_area.py
@@ -4,3 +4,7 @@ def calcular_area_esfera(raio):
     """Calcula a área da superfície de uma esfera dado o seu raio."""
     area = 4 * math.pi * (raio ** 2)
     return area
+
+# Entrada do usuário
+try:
+    raio_input = float(input("Digite o raio da esfera: "))
