3f89490b76aefa6c02a4587f475bed5ce8461950
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-03 12:19:38 -0300

    Solicita o raio ao usuário

--- esfera_volume.py
+++ esfera_volume.py
@@ -4,3 +4,7 @@ def calcular_volume_esfera(raio):
     # Fórmula: (4/3) * pi * r^3
     volume = (4/3) * math.pi * (raio ** 3)
     return volume
+
+# Solicita o raio ao usuário
+try:
+    r = float(input("Digite o raio da esfera: "))
