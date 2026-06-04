8af8fc13e078789352fc2c00de64781756a42fc8
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-04 20:10:19 -0300

    try

--- trape_area.py
+++ trape_area.py
@@ -4,3 +4,8 @@ def calcular_area_trapezio(base_maior, base_menor, altura):
 
 # Entrada de dados
 print("--- Calculadora de Área de Trapézio ---")
+
+try:
+    b_maior = float(input("Digite a base maior: "))
+    b_menor = float(input("Digite a base menor: "))
+    h = float(input("Digite a altura: "))
