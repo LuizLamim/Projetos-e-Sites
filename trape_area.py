16ed0a33136fe7e6cb96ac6ebb89f34309ab8e5b
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-04 20:09:50 -0300

    Entrada de dados

--- trape_area.py
+++ trape_area.py
@@ -1,3 +1,6 @@
 def calcular_area_trapezio(base_maior, base_menor, altura):
     """Calcula a área de um trapézio dado as bases e a altura."""
     return ((base_maior + base_menor) * altura) / 2
+
+# Entrada de dados
+print("--- Calculadora de Área de Trapézio ---")
