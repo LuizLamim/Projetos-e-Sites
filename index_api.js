930912f4412eae0b78c5a4ac2f53ff9617c6d6b4
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-08 18:07:08 -0300

    {}

--- index_api.js
+++ index_api.js
@@ -1,4 +1,6 @@
 // Usando a API pública JSONPlaceholder para demonstrar a conexão
 const API_URL = 'https://jsonplaceholder.typicode.com/posts/1';
 
-async function buscarDadosDaApi()
+async function buscarDadosDaApi(){
+    
+}
