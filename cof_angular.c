59a3228a6db7cc5018eba5834f201e15e8c0189f
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-09 19:40:36 -0300

    Cálculo e verificação

--- cof_angular.c
+++ cof_angular.c
@@ -3,5 +3,25 @@
 int main() {
     float x1, y1, x2, y2, m;
 
+    printf("--- Calculadora de Coeficiente Angular ---\n");
+    
+    // Entrada de dados
+    printf("Digite a coordenada x1: ");
+    scanf("%f", &x1);
+    printf("Digite a coordenada y1: ");
+    scanf("%f", &y1);
+    printf("Digite a coordenada x2: ");
+    scanf("%f", &x2);
+    printf("Digite a coordenada y2: ");
+    scanf("%f", &y2);
+
+    // Cálculo e verificação
+    if (x2 - x1 == 0) {
+        printf("\nErro: A reta e vertical. O coeficiente angular e indefinido.\n");
+    } else {
+        m = (y2 - y1) / (x2 - x1);
+        printf("\nO coeficiente angular (m) e: %.2f\n", m);
+    }
+
     return 0;
 }
