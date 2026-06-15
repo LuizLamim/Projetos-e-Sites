7f3a4a1222d8a51dec562540917084f004b0cfb3
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-15 14:51:23 -0300

    Varre cada pixel da imagem

--- fractal_plot.c
+++ fractal_plot.c
@@ -6,7 +6,7 @@
 #define HEIGHT 900
 #define MAX_ITER 1000
 
-int main(){
+int main() {
     // Abre o arquivo para salvar a imagem
     FILE *fp = fopen("mandelbrot.ppm", "wb");
     if (fp == NULL) {
@@ -27,6 +27,52 @@ int main(){
 
     printf("Gerando o fractal... Por favor, aguarde.\n");
 
+    // Varre cada pixel da imagem
+    for (int y = 0; y < HEIGHT; y++) {
+        double c_im = max_im - y * im_factor; // Componente imaginária
+
+        for (int x = 0; x < WIDTH; x++) {
+            double c_re = min_re + x * re_factor; // Componente real
+
+            // Z = Z^2 + C (Começando com Z = 0)
+            double z_re = 0.0, z_im = 0.0;
+            int iter = 0;
+            
+            // Variáveis para otimizar o cálculo (evita multiplicações repetidas)
+            double z_re2 = 0.0, z_im2 = 0.0;
+
+            // Laço principal de Mandelbrot
+            while (z_re2 + z_im2 <= 4.0 && iter < MAX_ITER) {
+                z_im = 2.0 * z_re * z_im + c_im;
+                z_re = z_re2 - z_im2 + c_re;
+                
+                z_re2 = z_re * z_re;
+                z_im2 = z_im * z_im;
+                iter++;
+            }
+
+            // Colorização baseada no número de iterações
+            unsigned char color[3];
+
+            if (iter == MAX_ITER) {
+                // Interior do conjunto (Preto)
+                color[0] = 0;
+                color[1] = 0;
+                color[2] = 0;
+            } else {
+                // Gradiente de cores psicodélico para as bordas
+                color[0] = (iter * 7) % 255;  // Vermelho
+                color[1] = (iter * 13) % 255; // Verde
+                color[2] = (iter * 23) % 255; // Azul
+            }
+
+            // Escreve os 3 bytes (RGB) do pixel no arquivo
+            fwrite(color, 1, 3, fp);
+        }
+    }
+
+    fclose(fp);
+    printf("Pronto! O arquivo 'mandelbrot.ppm' foi gerado com sucesso.\n");
 
     return 0;
 }
