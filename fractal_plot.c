93ccac77e19e78056ea946274282ee42733161ea
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-15 14:51:03 -0300

    Abre o arquivo para salvar a imagem,  Cabeçalho do formato PPM (P6 significa binário colorido), Limites do plano complexo para focar no fractal, e Fatores de escala

--- fractal_plot.c
+++ fractal_plot.c
@@ -14,6 +14,19 @@ int main(){
         return 1;
     }
 
+    // Cabeçalho do formato PPM (P6 significa binário colorido)
+    fprintf(fp, "P6\n%d %d\n255\n", WIDTH, HEIGHT);
+
+    // Limites do plano complexo para focar no fractal
+    double min_re = -2.0, max_re = 1.0;
+    double min_im = -1.2, max_im = 1.2;
+
+    // Fatores de escala
+    double re_factor = (max_re - min_re) / (WIDTH - 1);
+    double im_factor = (max_im - min_im) / (HEIGHT - 1);
+
+    printf("Gerando o fractal... Por favor, aguarde.\n");
+
 
     return 0;
 }
