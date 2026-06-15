ca948d9f2065803cd17efa19441967d0334cad7a
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-14 21:38:41 -0300

    Função para setar um número de precisão arbitrária com um valor inteiro pequeno

--- pi.c
+++ pi.c
@@ -2,3 +2,11 @@
 #include <stdlib.h>
 
 #define DIGITS 105 // Calculamos um pouco mais para evitar erros de arredondamento
+
+// Função para setar um número de precisão arbitrária com um valor inteiro pequeno
+void set_val(int *arr, int val) {
+    arr[0] = val;
+    for (int i = 1; i < DIGITS; i++) {
+        arr[i] = 0;
+    }
+}
