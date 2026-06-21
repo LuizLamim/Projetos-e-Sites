5729e1b9d0d226cc759436961ff509d2225beb1d
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-21 19:00:06 -0300

    Configurações Iniciais da Simulação

--- anima_electric_field.py
+++ anima_electric_field.py
@@ -1,3 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+
+# --- Configurações Iniciais da Simulação ---
+# Constante eletrostática (normalizada para 1 para simplificar a escala)
+K = 1.0  
+
+# Configuração do espaço (Grade de pontos onde o campo será calculado)
+X, Y = np.meshgrid(np.linspace(-5, 5, 20), np.linspace(-5, 5, 20))
