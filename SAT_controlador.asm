a7401796c03a6efa2be1f06073be40f5705a4ab1
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-23 18:05:04 -0300

    Global Start

--- SAT_controlador.asm
+++ SAT_controlador.asm
@@ -6,3 +6,6 @@ section .data
     ; Limiares de segurança
     ALTITUDE_CRITICA dw 350     ; Se cair abaixo disso, o satélite reentra na atmosfera
     BATERIA_BAIXA    db 20      ; Se cair abaixo disso, desliga sistemas não-essenciais
+
+section .text
+    global _start
