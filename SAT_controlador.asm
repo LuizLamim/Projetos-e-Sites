966d0cba86e7219b9f66588ac6ccc9235a98c2bb
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-23 18:04:20 -0300

    Simulando os sensores do satélite e Limiares de segurança

---
+++ SAT_controlador.asm
@@ -0,0 +1,8 @@
+section .data
+    ; Simulando os sensores do satélite (valores em memória)
+    altitude        dw 400      ; Altitude atual em km (ex: Órbita Baixa - LEO)
+    energia         db 15       ; Nível de bateria (0 a 100%)
+    
+    ; Limiares de segurança
+    ALTITUDE_CRITICA dw 350     ; Se cair abaixo disso, o satélite reentra na atmosfera
+    BATERIA_BAIXA    db 20      ; Se cair abaixo disso, desliga sistemas não-essenciais
