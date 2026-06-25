820a2f52499c570ca5bb0151a917c34c39cfc50d
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-25 18:30:04 -0300

    Recebe os coeficientes do usuário

--- x_vertice_2_grau.py
+++ x_vertice_2_grau.py
@@ -1,5 +1,9 @@
 def calcular_x_vertice():
 
     try:
+        # Recebe os coeficientes do usuário
+        a = float(input("Digite o valor de a: "))
+        b = float(input("Digite o valor de b: "))
+        c = float(input("Digite o valor de c: ")) # O 'c' não afeta o Xv, mas define a função
 
     except ValueError:
