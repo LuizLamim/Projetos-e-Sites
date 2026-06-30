26c9e036eadadf1a14471c932f0800601873cce4
LuizLamim <luiz.britohenrique@hotmail.com>
2026-06-30 20:13:51 -0300

    Função para verificar se um número é primo

--- primos_gemeos_numeros.c
+++ primos_gemeos_numeros.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+
+// Função para verificar se um número é primo
+bool eh_primo(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false; // Elimina os pares de cara
+
+    // Testa os divisores ímpares até a raiz quadrada de n
+    int limite = (int)sqrt(n);
+    for (int i = 3; i <= limite; i += 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
