e77913920842fe496a501b527571b9f274d5b37a
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-03 18:15:01 -0300

    4

--- multi_3.py
+++ multi_3.py
@@ -2,3 +2,6 @@
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 num3 = float(input("Digite o terceiro número: "))
+
+# Realizando a multiplicação
+resultado = num1 * num2 * num3
