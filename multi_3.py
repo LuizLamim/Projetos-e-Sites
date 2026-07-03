4011836edab00079fed02f47be32c3ae1ffd0a31
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-03 18:14:45 -0300

    3

--- multi_3.py
+++ multi_3.py
@@ -1,3 +1,4 @@
 # Solicitando os três números ao usuário
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
