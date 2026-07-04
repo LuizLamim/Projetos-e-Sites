b3e1fe559a322ec0404613938bfe79989e80e6a8
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-04 20:58:21 -0300

    3

--- area_trapezio.py
+++ area_trapezio.py
@@ -4,3 +4,5 @@ print("--- Calculadora de Área do Trapézio ---")
 
 # Solicitando os dados ao usuário
 base_maior = float(input("Digite o comprimento da base maior (B): "))
+base_menor = float(input("Digite o comprimento da base menor (b): "))
+altura = float(input("Digite a altura do trapézio (h): "))
