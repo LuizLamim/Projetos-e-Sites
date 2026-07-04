b68c8a3e5f3d940272192da01664e766ec7db331
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-04 20:58:10 -0300

    Solicitando os dados ao usuário

--- area_trapezio.py
+++ area_trapezio.py
@@ -1,3 +1,6 @@
 # Programa para calcular a área de um trapézio
 
 print("--- Calculadora de Área do Trapézio ---")
+
+# Solicitando os dados ao usuário
+base_maior = float(input("Digite o comprimento da base maior (B): "))
