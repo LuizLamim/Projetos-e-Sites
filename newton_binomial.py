7f39a3f5afa5dd57a26235d9ffd7bfa1ed78b40e
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-05 21:01:46 -0300

    4

--- newton_binomial.py
+++ newton_binomial.py
@@ -12,3 +12,28 @@ def binomio_newton(n, var1='x', var2='y'):
         return "1"
     
     termos = []
+
+    for k in range(n + 1):
+        # Calcula o coeficiente binomial: n! / (k! * (n-k)!)
+        coeficiente = math.comb(n, k)
+        
+        # Define as potências de cada variável
+        p_var1 = n - k
+        p_var2 = k
+        
+        # Formata a parte das variáveis
+        str_var1 = ""
+        if p_var1 > 0:
+            str_var1 = var1 if p_var1 == 1 else f"{var1}^{p_var1}"
+            
+        str_var2 = ""
+        if p_var2 > 0:
+            str_var2 = var2 if p_var2 == 1 else f"{var2}^{p_var2}"
+            
+        # Junta o coeficiente com as variáveis
+        if coeficiente == 1 and (p_var1 > 0 or p_var2 > 0):
+            termo_atual = f"{str_var1}{str_var2}"
+        else:
+            termo_atual = f"{coeficiente}{str_var1}{str_var2}"
+            
+        termos.append(termo_atual)
