0967dad9a3e6d63e82d4a6275e0c410b5e1b7314
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-05 21:02:10 -0300

    5

--- newton_binomial.py
+++ newton_binomial.py
@@ -37,3 +37,19 @@ def binomio_newton(n, var1='x', var2='y'):
             termo_atual = f"{coeficiente}{str_var1}{str_var2}"
             
         termos.append(termo_atual)
+
+        # Une todos os termos com o sinal de adição
+    expansao = " + ".join(termos)
+    return expansao
+
+# --- Teste do Programa ---
+if __name__ == "__main__":
+    print("--- Gerador de Binômio de Newton ---")
+    try:
+        expoente = int(input("Digite o valor do expoente (n): "))
+        resultado = binomio_newton(expoente)
+        
+        print(\nf"Resultado para (x + y)^{expoente}:")
+        print(resultado)
+    except ValueError:
+        print("Por favor, digite um número inteiro válido.")
