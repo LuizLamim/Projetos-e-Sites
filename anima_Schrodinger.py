30218400ce0b89ac9767d234aff5d85366787f34
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-06 19:57:44 -0300

    2

--- anima_Schrodinger.py
+++ anima_Schrodinger.py
@@ -1,3 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+
+
+# Definindo constantes (unidades naturais: hbar = 1, m = 1, omega = 1)
+x = np.linspace(-5, 5, 500)
+
+# Funções de onda dos autoestados (Estado fundamental e 1º estado excitado)
+def psi_0(x):
+    """Estado fundamental do oscilador harmônico"""
+    return (1.0 / np.pi)**0.25 * np.exp(-x**2 / 2.0)
+
+def psi_1(x):
+    """Primeiro estado excitado"""
+    return (1.0 / np.pi)**0.25 * np.sqrt(2.0) * x * np.exp(-x**2 / 2.0)
