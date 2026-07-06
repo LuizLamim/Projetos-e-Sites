bb395329ca44e8fde840c3db627c902522f09992
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-05 21:01:28 -0300

    3

--- newton_binomial.py
+++ newton_binomial.py
@@ -1,3 +1,14 @@
 import math
 
 def binomio_newton(n, var1='x', var2='y'):
+
+    """
+    Gera e exibe a expansão do Binômio de Newton para (var1 + var2)^n
+    """
+    if n < 0:
+        return "Por favor, insira um expoente inteiro não-negativo (n >= 0)."
+    
+    if n == 0:
+        return "1"
+    
+    termos = []
