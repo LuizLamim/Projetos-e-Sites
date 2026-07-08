a4b54e7294d3c6e5021a54474ae5d7a019670344
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-07 21:06:38 -0300

    Calculando a equação

--- x_pow_15.py
+++ x_pow_15.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 # 1. Definindo o intervalo de x
 # Geramos 400 pontos linearmente espaçados entre -1.5 e 1.5
 x = np.linspace(-1.5, 1.5, 400)
+
+# 2. Calculando a equação
+y = x**15
