10c66ed7fb0f4670e7aee7d3cc29d54d6d3b0fd2
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-09 20:19:00 -0300

    2

--- plot_x_4.py
+++ plot_x_4.py
@@ -1,2 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+# 1. Definir o intervalo de valores para o eixo X
+# np.linspace cria 400 pontos distribuídos uniformemente entre -5 e 5
+x = np.linspace(-5, 5, 400)
