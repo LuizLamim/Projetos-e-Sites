b7beff3328bd01ed602f65fd0018bee111a7691c
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-08 22:23:51 -0300

    3

--- plot_x_3.py
+++ plot_x_3.py
@@ -7,3 +7,6 @@ import matplotlib.pyplot as plt
 # 1. Define os valores de x (de -10 a 10, gerando 400 pontos para uma curva suave)
 x = np.linspace(-10, 10, 400)
 
+# 2. Calcula os valores de y para a função x^3
+y = x**3
+
