a508fcc644ae97e7fbdc43660c7c22b01e261ec3
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-08 22:23:36 -0300

    2

--- plot_x_3.py
+++ plot_x_3.py
@@ -1,2 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 1. Define os valores de x (de -10 a 10, gerando 400 pontos para uma curva suave)
+x = np.linspace(-10, 10, 400)
+
