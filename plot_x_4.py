ad0903787f9b48024f49dcbcae5b7f9d8a405683
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-09 20:19:10 -0300

    3

--- plot_x_4.py
+++ plot_x_4.py
@@ -4,3 +4,6 @@ import numpy as np
 # 1. Definir o intervalo de valores para o eixo X
 # np.linspace cria 400 pontos distribuídos uniformemente entre -5 e 5
 x = np.linspace(-5, 5, 400)
+
+# 2. Calcular os valores do eixo Y (a função x^4)
+y = x**4
