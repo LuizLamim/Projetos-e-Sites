595858f74cc0771ad362419d73f9637d5d9d1d1d
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-12 20:10:13 -0300

    3

--- soma_frac.py
+++ soma_frac.py
@@ -1,3 +1,8 @@
 from fractions import Fraction
 
 def somar_fracoes():
+    print("--- Programa para Somar Frações ---")
+    
+    # Solicita ao usuário que insira as frações
+    frac1_input = input("Digite a primeira fração (ex: 1/2): ")
+    frac2_input = input("Digite a segunda fração (ex: 3/4): ")
