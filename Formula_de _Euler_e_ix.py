97e8434e5766ba4c382f58d5c125e228ae5972af
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-13 21:41:10 -0300

    Separar as partes real e imaginária

--- Formula_de _Euler_e_ix.py
+++ Formula_de _Euler_e_ix.py
@@ -7,3 +7,7 @@ x = np.linspace(0, 4 * np.pi, 500)
 # 2. Calcular a função e^(ix)
 # Em Python, 'j' é usado para representar a unidade imaginária (i)
 y = np.exp(1j * x)
+
+# 3. Separar as partes real e imaginária
+parte_real = np.real(y)  # Equivalente a cos(x)
+parte_imag = np.imag(y)  # Equivalente a sin(x)
