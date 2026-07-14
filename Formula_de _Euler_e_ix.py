e77a3f77d825f43d355d7dd62fd60ce18c5a0746
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-13 21:40:45 -0300

    Calcular a função e^(ix)

--- Formula_de _Euler_e_ix.py
+++ Formula_de _Euler_e_ix.py
@@ -3,3 +3,7 @@ import matplotlib.pyplot as plt
 
 # 1. Definir o intervalo de x (de 0 a 4*pi para mostrar dois ciclos completos)
 x = np.linspace(0, 4 * np.pi, 500)
+
+# 2. Calcular a função e^(ix)
+# Em Python, 'j' é usado para representar a unidade imaginária (i)
+y = np.exp(1j * x)
