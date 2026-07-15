997f2ca0d74081371bfdad344382f3ded8d6d23e
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-14 23:27:39 -0300

    2

--- cpu.asm
+++ cpu.asm
@@ -2,3 +2,6 @@ section .data
     ; Seção de dados inicializados
     msg db 'Operacoes da CPU concluidas com sucesso!', 0xA  ; String com quebra de linha (0xA)
     len equ $ - msg                                         ; Calcula o tamanho da string
+
+section .text
+    global _start   ; Define o ponto de entrada do programa
