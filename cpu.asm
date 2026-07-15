23118a4c6ceafd0561020faae9abd64a0c7d2ec7
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-14 23:28:01 -0300

    3

--- cpu.asm
+++ cpu.asm
@@ -5,3 +5,10 @@ section .data
 
 section .text
     global _start   ; Define o ponto de entrada do programa
+
+_start:
+    ; ==========================================
+    ; 1. MOVIMENTAÇÃO DE DADOS (Load/Store)
+    ; ==========================================
+    mov rax, 15      ; Move o valor 15 para o registrador RAX
+    mov rbx, 10      ; Move o valor 10 para o registrador RBX
