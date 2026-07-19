ea1c9c40501adda9fc9f6fdaa90c3a89907db37b
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-18 22:42:47 -0300

    Ângulos

--- hectagono_2.py
+++ hectagono_2.py
@@ -3,3 +3,7 @@ import numpy as np
 
 # Definindo o número de lados do hectágono
 lados = 100
+
+# Calculando os ângulos para cada um dos 100 vértices (de 0 a 2*pi)
+# Adicionamos um ponto extra no final para fechar o polígono perfeitamente
+angulos = np.linspace(0, 2 * np.pi, lados + 1)
