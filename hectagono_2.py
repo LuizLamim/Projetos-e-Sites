c4e7d1daa0aed967d9793637cfc56632728c3627
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-18 22:42:58 -0300

    Calculando as coordenadas X e Y

--- hectagono_2.py
+++ hectagono_2.py
@@ -7,3 +7,7 @@ lados = 100
 # Calculando os ângulos para cada um dos 100 vértices (de 0 a 2*pi)
 # Adicionamos um ponto extra no final para fechar o polígono perfeitamente
 angulos = np.linspace(0, 2 * np.pi, lados + 1)
+
+# Calculando as coordenadas X e Y usando o círculo unitário (raio = 1)
+x = np.cos(angulos)
+y = np.sin(angulos)
