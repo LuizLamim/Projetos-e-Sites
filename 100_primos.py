6a5448dd0c2bd2560b7ed46fcdd6a35ef003915f
LuizLamim <112779563+LuizLamim@users.noreply.github.com>
2026-07-19 22:38:32 -0300

    Atualizar o 100_primos.py
    101 primos

--- 100_primos.py
+++ 100_primos.py
@@ -12,7 +12,7 @@ numeros_primos = []
 numero_atual = 2
 
 # Loop continua até a lista ter 100 elementos
-while len(numeros_primos) < 100:
+while len(numeros_primos) < 101:
     if eh_primo(numero_atual):
         numeros_primos.append(numero_atual)
     numero_atual += 1
