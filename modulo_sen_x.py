978fb09a282ceea4a0c43445511b2c64ef1c277e
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-21 19:02:10 -0300

    f(x) = | sen (x) |

--- modulo_sen_x.py
+++ modulo_sen_x.py
@@ -1,2 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# 1. Definir o intervalo de x (de -2π a 2π, com 1000 pontos para manter a curva suave)
+x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
