468e8c45170d4a86c76b75e13b7c20436d5e14a1
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-21 19:03:41 -0300

    y = |sen(x)|

--- modulo_sen_x.py
+++ modulo_sen_x.py
@@ -3,3 +3,7 @@ import matplotlib.pyplot as plt
 
 # 1. Definir o intervalo de x (de -2π a 2π, com 1000 pontos para manter a curva suave)
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+
+# 2. Calcular os valores de y = |sen(x)|
+# np.sin(x) calcula o seno, e np.abs() aplica o módulo (valor absoluto)
+y = np.abs(np.sin(x))
