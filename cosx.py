38236c4ae0864220e5b4b47da42544d4d69e9711
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-23 23:42:38 -0300

    Definir o intervalo do eixo x

--- cosx.py
+++ cosx.py
@@ -1,2 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# 1. Definir o intervalo do eixo x (de -2π a 2π, com 1000 pontos para suavizar a curva)
+x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
