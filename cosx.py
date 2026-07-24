6a3faf0bbe00b86ad741325d31abe69e895b6f01
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-23 23:43:15 -0300

    Criar e configurar o gráfico

--- cosx.py
+++ cosx.py
@@ -6,3 +6,7 @@ x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
 
 # 2. Calcular o valor de y = |cos(x)|
 y = np.abs(np.cos(x))
+
+# 3. Criar e configurar o gráfico
+plt.figure(figsize=(10, 5))
+plt.plot(x, y, color='blue', linewidth=2, label=r'$y = |\cos(x)|$')
