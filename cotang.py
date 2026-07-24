8b72a04dedb39e06b12c137849da01b0305a6dac
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-24 18:12:35 -0300

    Calcular a cotangente

--- cotang.py
+++ cotang.py
@@ -3,3 +3,9 @@ import matplotlib.pyplot as plt
 
 # 1. Definir o intervalo de x (de -2π a 2π com 1000 pontos para suavidade)
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+
+
+# 2. Calcular a cotangente (1 / tangente)
+# Usamos um pequeno aviso de ignorar divisão por zero para evitar poluir o terminal
+with np.errstate(divide='ignore'):
+    y = 1 / np.tan(x)
