0fe2e9ad891524ea1c89c2da6a2ab30857887115
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-23 23:42:50 -0300

    Calcular o valor de y = |cos(x)|

--- cosx.py
+++ cosx.py
@@ -3,3 +3,6 @@ import matplotlib.pyplot as plt
 
 # 1. Definir o intervalo do eixo x (de -2π a 2π, com 1000 pontos para suavizar a curva)
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+
+# 2. Calcular o valor de y = |cos(x)|
+y = np.abs(np.cos(x))
