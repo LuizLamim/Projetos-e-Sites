6763daceda7f8b6b03d5a4d8a3ec3c0e95db793b
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-27 17:11:16 -0300

    Cria uma grade de coordenadas no plano complexo

--- fractal.py
+++ fractal.py
@@ -2,3 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def gerar_mandelbrot(xmin, xmax, ymin, ymax, largura, altura, max_iter):
+    # Cria uma grade de coordenadas no plano complexo
+    r1 = np.linspace(xmin, xmax, largura)
+    r2 = np.linspace(ymin, ymax, altura)
+    X, Y = np.meshgrid(r1, r2)
