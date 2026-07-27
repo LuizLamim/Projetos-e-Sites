4b39196ee824908d350e443e9bac77948c0a4fbe
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-27 17:12:02 -0300

    Matriz para armazenar a "cor" de cada pixel.

--- fractal.py
+++ fractal.py
@@ -10,3 +10,18 @@ def gerar_mandelbrot(xmin, xmax, ymin, ymax, largura, altura, max_iter):
     # C é o nosso ponto no plano (Parte Real + Parte Imaginária)
     C = X + 1j * Y
     Z = np.zeros_like(C)
+
+    # Matriz para armazenar a "cor" de cada pixel (número de iterações)
+    fractal = np.zeros(C.shape, dtype=int)
+    
+    for i in range(max_iter):
+        # Aplica a fórmula apenas aos pontos que ainda não "escaparam" (raio <= 2)
+        mascara = np.abs(Z) <= 2
+        
+        # A famosa equação do fractal: Z = Z² + C
+        Z[mascara] = Z[mascara]**2 + C[mascara]
+        
+        # Registra em qual iteração o ponto estava
+        fractal[mascara] = i
+        
+    return fractal
