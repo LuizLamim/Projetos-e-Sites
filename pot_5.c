ea2063987e6ed7e071b9bb61da37c15fed892106
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-28 19:02:34 -0300

    Calcula a quinta potência multiplicando o número por ele mesmo

--- pot_5.c
+++ pot_5.c
@@ -7,4 +7,7 @@ int main(){
     printf("Digite um número para calcular sua quinta potência: ");
     scanf("%lf", &numero);
 
+    // Calcula a quinta potência multiplicando o número por ele mesmo
+    resultado = numero * numero * numero * numero * numero;
+
 }
