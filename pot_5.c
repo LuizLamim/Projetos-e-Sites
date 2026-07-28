4a3a31a45621b65b5630b16ef68fea4efb8aae08
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-28 19:02:22 -0300

    Solicita a entrada do usuário

--- pot_5.c
+++ pot_5.c
@@ -3,4 +3,8 @@
 int main(){
     double numero, resultado;
 
+    // Solicita a entrada do usuário
+    printf("Digite um número para calcular sua quinta potência: ");
+    scanf("%lf", &numero);
+
 }
