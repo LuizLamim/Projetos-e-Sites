646c15879b239280362cba288a88c9d840271cac
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-29 18:24:37 -0300

    Função auxiliar para imprimir os dígitos e formatar com a vírgula

--- pi_153_casas.c
+++ pi_153_casas.c
@@ -2,3 +2,13 @@
 
 // Variável global para rastrear onde colocar a vírgula
 int total_impresso = 0;
+
+// Função auxiliar para imprimir os dígitos e formatar com a vírgula
+void imprimir_digito(int digito) {
+    printf("%d", digito);
+    total_impresso++;
+    // Imprime a vírgula logo após o '3' inicial
+    if (total_impresso == 1) {
+        printf(".");
+    }
+}
