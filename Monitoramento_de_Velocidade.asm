cd738a9daee90cd05fcbbdabfb43bb44b82f1a81
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-31 17:45:23 -0300

    Section Data

--- Monitoramento_de_Velocidade.asm
+++ Monitoramento_de_Velocidade.asm
@@ -2,3 +2,8 @@
 ; Sistema de Monitoramento de Velocidade (Overspeed Warning)
 ; Arquitetura: x86 (Exemplo de sistema embarcado legado)
 ; =====================================================================
+
+section .data
+    VNE_LIMIT equ 350       ; Limite de velocidade estrutural em nós (knots)
+    PORT_PITOT equ 0x300    ; Endereço de I/O mapeado para o sensor Pitot
+    PORT_ALARM equ 0x302    ; Endereço de I/O mapeado para o alarme no painel
