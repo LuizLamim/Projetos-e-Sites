bc693aa568cf898467f2200afc5556c0f890727b
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-31 17:44:53 -0300

    Sistema de Monitoramento de Velocidade (Overspeed Warning)

---
+++ Monitoramento_de_Velocidade.asm
@@ -0,0 +1,4 @@
+; =====================================================================
+; Sistema de Monitoramento de Velocidade (Overspeed Warning)
+; Arquitetura: x86 (Exemplo de sistema embarcado legado)
+; =====================================================================
