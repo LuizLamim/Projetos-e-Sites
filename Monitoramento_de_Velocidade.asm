783e919359745b587c2a966d7e001671692d2a41
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-31 17:45:47 -0300

    Section Text.

--- Monitoramento_de_Velocidade.asm
+++ Monitoramento_de_Velocidade.asm
@@ -7,3 +7,9 @@ section .data
     VNE_LIMIT equ 350       ; Limite de velocidade estrutural em nós (knots)
     PORT_PITOT equ 0x300    ; Endereço de I/O mapeado para o sensor Pitot
     PORT_ALARM equ 0x302    ; Endereço de I/O mapeado para o alarme no painel
+
+section .text
+    global _start
+
+_start:
+    ; Ponto de entrada do sistema
