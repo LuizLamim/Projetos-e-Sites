ce4edf48c03bac3131be6f6537db0c23300e797b
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-01 18:34:57 -0300

    Mapeamento de Hardware

--- Monitoramento_de_Velocidade.c
+++ Monitoramento_de_Velocidade.c
@@ -2,3 +2,10 @@
 
 // 1. DEFINIÇÕES DO SISTEMA
 #define VNE_LIMIT 350  // Velocidade estrutural máxima (knots)
+
+// Mapeamento de Hardware (Memory-Mapped I/O)
+// Os ponteiros apontam para os endereços físicos do hardware no barramento.
+// A palavra-chave 'volatile' é obrigatória: ela avisa o compilador para 
+// NUNCA otimizar essas variáveis, pois o valor pode ser alterado pelo hardware a qualquer momento.
+#define SENSOR_PITOT (*(volatile uint16_t*)0x0300)
+#define ALARME_PAINEL (*(volatile uint8_t*)0x0302)
